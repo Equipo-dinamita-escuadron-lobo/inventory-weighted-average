2f02c6e6080ef42f8fc69e5fe3bece740e063c06
JulianRuano <julianrmajin@gmail.com>
2025-08-20 16:36:22 -0500

    feat: Update Kardex return methods to include unit price and product ID in fact code queries

--- src/main/java/com/kardex/domain/port/IKardexQueryRepositoryPort.java
+++ src/main/java/com/kardex/domain/port/IKardexQueryRepositoryPort.java
@@ -11,6 +11,6 @@ import com.kardex.domain.model.Kardex;
 public interface IKardexQueryRepositoryPort {
     Page<Kardex> findProductId(Long productId, Pageable pageable);
     Page<Kardex> findProductIdAndDate(Long productId, Pageable pageable, LocalDate startDate, LocalDate endDate);
-    List<Kardex> findByFactCode(Long factCode);
+    List<Kardex> findByFactCodeAndProductId(Long factCode, Long productId);
     Kardex getLatestKardexByProductId(Long productId);
 }
