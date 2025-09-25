5f33a1fe365c521e20aa908db4ff8b21a45a70dc
JulianRM <julianrmajin@gmail.com>
2025-09-25 15:16:19 -0500

    feat: Remove security settings for testing and simplify context testing

--- src/test/java/com/kardex/KardexApplicationTests.java
+++ src/test/java/com/kardex/KardexApplicationTests.java
@@ -1,20 +1,9 @@
 package com.kardex;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-@Disabled
-@ImportAutoConfiguration(exclude = {
-    org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class
-})
 class KardexApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
